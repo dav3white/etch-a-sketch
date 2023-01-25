a14ce0d0a9c2e3d82ae0a22a90f174c01693dbfd
Dave White <dav3white@gmail.com>
2023-01-25 22:10:25 +0000

    Add code to allow user grid size

--- javascript.js
+++ javascript.js
@@ -1,19 +1,32 @@
-// Code goes here
-const grid = document.querySelector('#grid');
-const gridsize = 64;
-const cellsize = 1/Math.sqrt(gridsize);
+const button = document.getElementById('squares');
+button.addEventListener('click', setSquares);
 
-for (i=1; i<=64; i++) {
-    const cell = document.createElement('div');
-    cell.id = i;
-    cell.classList.add('cell');
-/* Use this line to dynamically set width and height of cell   
-cell.setAttribute("style", width) */
-    cell.addEventListener('mouseenter', () => { changeColor(cell.id); });
-    grid.appendChild(cell);
+function setSquares() {
+    removeChildElements();
+    var numsquares = Math.pow(window.prompt("Enter number of squares on each side (<100)"), 2);
+    const grid = document.querySelector('#grid');
+    console.log('numsquares: ' + numsquares);
+    const cellsize = (960*1/Math.sqrt(numsquares))-2;
+    console.log('Cell Size: ' + cellsize);
+    for (i=1; i<=numsquares; i++) {
+        const cell = document.createElement('div');
+        cell.id = i;
+        cell.setAttribute("style", "width:"+cellsize+"px;height:"+cellsize+"px");
+        cell.classList.add('cell');
+        cell.addEventListener('mouseenter', () => { changeColor(cell.id); });
+        grid.appendChild(cell);
+    }
 }
 
 function changeColor(cell) {
     document.getElementById(cell).style.backgroundColor = "red";
 }
 
+function removeChildElements() {
+    var childElements = document.getElementById("grid");
+    var delChild = childElements.lastChild;
+    while (delChild) {
+     childElements.removeChild(delChild);
+     delChild = childElements.lastChild;
+     }
+}
