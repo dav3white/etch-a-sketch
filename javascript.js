54029d7fdfcd5300ec96bc01ea8861aaaeff0d76
Dave White <dav3white@gmail.com>
2023-03-05 07:58:08 +0000

    Tidy up code

--- javascript.js
+++ javascript.js
@@ -1,16 +1,16 @@
-const button = document.getElementById('squares');
-button.addEventListener('click', setSquares);
+const button = document.getElementById('squares').addEventListener('click', buildGrid);
 
-function setSquares() {
-    removeChildElements();
-    var numsquares = Math.pow(window.prompt("Enter number of squares on each side (<100)"), 2);
+function buildGrid() {
+    resetGrid();
+    var sideLength = Math.pow(window.prompt("Enter number of squares on each side (<100)"), 2);
     const grid = document.querySelector('#grid');
-    gridsize = grid.offsetHeight;
-    const cellsize = (gridsize*1/Math.sqrt(numsquares))-2;
-    for (i=1; i<=numsquares; i++) {
+    gridSize = 600;
+    grid.setAttribute("style", `width:${gridSize}px;height:${gridSize}px`);
+    const cellSize = (gridSize/Math.sqrt(sideLength))-2;
+    for (i=1; i<=sideLength; i++) {
         const cell = document.createElement('div');
         cell.id = i;
-        cell.setAttribute("style", "width:"+cellsize+"px;height:"+cellsize+"px");
+        cell.setAttribute("style", `width:${cellSize}px;height:${cellSize}px`);
         cell.classList.add('cell');
         cell.addEventListener('mouseenter', () => { changeColor(cell.id); });
         grid.appendChild(cell);
@@ -21,11 +21,11 @@ function changeColor(cell) {
     document.getElementById(cell).style.backgroundColor = "red";
 }
 
-function removeChildElements() {
-    var childElements = document.getElementById("grid");
-    var delChild = childElements.lastChild;
-    while (delChild) {
-     childElements.removeChild(delChild);
-     delChild = childElements.lastChild;
+function resetGrid() {
+    var grid = document.getElementById("grid");
+    var elementToRemove = grid.lastChild;
+    while (elementToRemove) {
+     grid.removeChild(elementToRemove);
+     elementToRemove = grid.lastChild;
      }
 }
